18e41824774ddbb211f08c0618feda2e895bbb1e
francoposa <franco@francoposa.io>
2021-11-28 16:52:14 -0800

    backend server working with chi and cobra/viper config from yaml file

---
+++ cmd/root.go
@@ -0,0 +1,50 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	cfgFile string
+	rootCmd = &cobra.Command{
+		Use:   "",
+		Short: "",
+		Long:  ``,
+		//Run: func(cmd *cobra.Command, args []string) {
+		//	// Do stuff here if root command can be used alone
+		//},
+	}
+)
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		panic(err)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().StringVar(
+		&cfgFile,
+		"config",
+		"",
+		"config file")
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	}
+
+	//viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	} else {
+		panic(err)
+	}
+
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/francoposa/go-csrf-examples/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
