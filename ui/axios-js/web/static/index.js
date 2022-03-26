52eb33a55698316c2ea6e711494baf1e91dbb589
francoposa <franco@francoposa.io>
2022-03-26 15:26:23 -0700

    flesh out axios frontend example, writing response values to HTML

--- ui/axios-js/web/static/index.js
+++ ui/axios-js/web/static/index.js
@@ -1,11 +1,19 @@
+// make GET request to backend on page load in order to obtain
+// a CSRF Token and load it into the Axios instance's headers
+// https://github.com/axios/axios#creating-an-instance
 const initializeAxiosInstance = async (url) => {
-    var csrfToken;
     try {
         let resp = await axios.get(url, {withCredentials: true});
         console.log(resp);
-        csrfToken = resp.headers["x-csrf-token"];
+        document.getElementById("get-request-full-response").innerHTML = JSON.stringify(resp);
+
+        let csrfToken = parseCSRFToken(resp);
         console.log(csrfToken);
+        document.getElementById("get-response-csrf-token").innerHTML = csrfToken;
+
         return axios.create({
+            // withCredentials must be true to in order for the browser
+            // to send cookies, which are necessary for CSRF verification
             withCredentials: true,
             headers: {"X-CSRF-Token": csrfToken}
         });
@@ -18,12 +26,23 @@ const post = async (axiosInstance, url) => {
     try {
         let resp = await axiosInstance.post(url);
         console.log(resp);
+        document.getElementById("post-request-full-response").innerHTML = JSON.stringify(resp);
     } catch (err) {
         console.log(err);
     }
 };
 
+
+const parseCSRFToken = (resp) => {
+    let csrfToken = resp.headers[csrfTokenHeader];
+    if (!csrfToken) {
+        csrfToken = resp.headers[csrfTokenHeader.toLowerCase()];
+    }
+    return csrfToken
+}
+
 const url = "http://localhost:8080/api";
+const csrfTokenHeader = "X-CSRF-Token";
 initializeAxiosInstance(url)
     .then(axiosInstance => {
         post(axiosInstance, url);
